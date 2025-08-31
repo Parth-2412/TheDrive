f58be2cd8a8674a540d9083743b7b93826698900
Parth2412 <parthnpatil06@gmail.com>
2025-08-31 19:01:17 +0530

    Uninstall tailwind

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,15 +1,15 @@
-/// <reference types="vitest" />
+d f/// <reference types="vitest" />
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     legacy(),
-    tailwindcss()
+
   ],
   resolve: {
     alias: {
